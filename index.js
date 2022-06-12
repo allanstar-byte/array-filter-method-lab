9272259a73b5e0bd4a505a0749c59199db11b7f2
allanstar-byte <allanmutisya263@gmail.com>
2022-06-12 13:22:47 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(name) {
+    for (const i of drivers) {
+        if (i === name) {
+            return i
+        } else {
+            return []
+        }
+    }
+}
+findMatching('y')
